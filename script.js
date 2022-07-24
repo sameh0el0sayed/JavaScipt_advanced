cfde4021363b96dfdbe16e7645db16bfefcbce90
sameh0el0sayed <samehsr.work@gmail.com>
2022-07-24 03:50:41 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// var aabb=10; //can reAssignment and can redeclare 
+// let //can reAssignment only and change type of data
+//console hold all of Assignment and type 
+
+const aa=true;
+
+console.log("%cStop! \n %cThis browser feature intended for developers only. ","color:Red;font-size:50px;text-align:center;text-shadow: 3px 1px black","");
+ 
+
+ 
+
+ 
+ 
+ 
