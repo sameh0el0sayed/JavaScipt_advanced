93f98f4884ca9e4f014ac35e27abd3665804166c
sameh0el0sayed <samehsr.work@gmail.com>
2022-08-06 21:37:11 +0300

    strings

---
+++ Strings_logical_operator.js
@@ -0,0 +1,15 @@
+let theName=`javascript`;
+// console.log(theName.charAt(1))
+// console.log(theName.trim().charAt(4).toUpperCase())
+///
+
+console.clear();
+console.log(theName.indexOf("a"));//1
+console.log(theName.lastIndexOf("a"));//index 3
+console.log(theName.slice(0,4  ))//java
+console.log(theName.slice(-6  ))//java
+console.log(theName.repeat(3))//java
+console.log(theName.split("\n"));
+
+console.log(theName.substr(4,4));
+
--- numbers.js
+++ numbers.js
@@ -1,4 +1,4 @@
-// this practise about the math and how to implement of numbers in es7
+// this practise about the Math and how to implement of numbers in es7
 
 //pre increment
 num=1;
@@ -29,7 +29,7 @@ a/=60;
 
 console.log(a);
 
-//Challenge
+//Challenge of uunary operators
 
 let d="-100";
 let e= "20";
@@ -39,6 +39,55 @@ let g=true;
 console.log('//Challenge');
 //console.log(-d*+e);//2000
 //console.log(-d + ++f + ++e * ++g)//173;
- 
 console.log(-d+ ++e + ++f + ++g)//154;
+
+//Number function
+//max safe value,max value on JS
+
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(Number.MAX_VALUE); 
+
+console.log(Number.isFinite(10/5))//true
+console.log(Number.isFinite(1/0));//false ,that is, it checks that the type of a given value is Number, and the number is neither positive Infinity
+console.log(Number.isInteger(100));//true
+console.log(Number.isInteger("100"));//false
+console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER))//true
+console.log(Number.isSafeInteger(Number.MAX_VALUE))//false
+console.log(Number.isNaN("Sameh" /20));//true, check the return value is NaN, don't check the value on fun
+ 
+//Math Objects
+
+console.log(Math.round(99.2));//round to 99
+console.log(Math.round(99.5));//round to 100
+ 
+console.log(Math.ceil(99.2));//max great number =100
+console.log(Math.floor(99.5));//min number =99
+console.log(Math.min(300,1,3,4));
+console.log(Math.max(300,1,3,4));
+
+console.log(Math.pow(2,4));//2^4
+console.log(Math.random(10,9));
+
+console.log(Math.trunc(99.55));// remove after . 99.555=99 only
+
+//Math Objects challenge
+
+let ma=1_00;
+let mb=2_00.5;
+let mc=1e2;
+let md=2.4;
+
+//min
+console.log(Math.trunc((Math.min(ma,mb,mc,md))));
+
+//10000 from ma,md
+console.log(Math.pow(ma,Math.trunc(md)));
+
+//mb ,md to get 66.67 string, 67 number
+console.log((Math.trunc(mb)/Math.ceil(md)).toFixed(2));//string 66.67
+console.log(Math.ceil(Math.trunc(mb)/Math.ceil(md)));//67
+
+
+
+
 
